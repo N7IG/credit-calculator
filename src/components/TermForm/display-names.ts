b7629deaa4f8e43d1b680d72fda0aaa8cceef02b
Nataliya Okuneva <nataliya_okuneva@epam.com>
2020-09-08 15:51:42 +0500

    feat: change display name

--- src/components/TermForm/display-names.ts
+++ src/components/TermForm/display-names.ts
@@ -1,5 +1,5 @@
 export const DISPLAY_NAMES_RUS = {
   TERM_LABEL: "Срок кредитования",
-  DIFF_RADIO: "Дифференцированные платежи",
-  ANNUITY_RADIO: "Аннуитет",
+  DIFF_RADIO: "Дифференцированный платеж",
+  ANNUITY_RADIO: "Аннуитетный платеж",
 };
